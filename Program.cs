998be451faaad315a0cd7beb033fad85718087a4
Georgiy <lplastique@gmail.com>
2022-09-21 20:24:48 +0300

    задача №4

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.Write("Введите первое число: ");
+// int num1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = int.Parse(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine("первое число больше второго");
+// }
+// else
+// {
+//     Console.WriteLine("второе число больше первого");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.Write("Введите первое число: ");
+// int num1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = int.Parse(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int num3 = int.Parse(Console.ReadLine());
+
+// int num1 = max;
+// if (num1 < num2)
+// {
+//     num2 = max;
+// }
+// else (num2 < num3)
+//  {
+//     num3 = max;
+//  }
+
+// Console.WriteLine($"Максимальное число = {max}");
